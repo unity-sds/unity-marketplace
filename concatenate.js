63b9bdc02cc723a75636353c32a11cdd81242154
Tom Barber <tom@spicule.co.uk>
2023-08-08 09:31:18 +0100

    Create concatenate.js

---
+++ concatenate.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const jsonfile = require('jsonfile');
+
+const directoryPath = 'unity-marketplace/applications';
+let manifest = [];
+
+const walkDirectory = (dir, callback) => {
+  fs.readdirSync(dir).forEach(file => {
+    const filepath = path.join(dir, file);
+    const stats = fs.statSync(filepath);
+    if (stats.isDirectory()) {
+      walkDirectory(filepath, callback);
+    } else if (path.extname(filepath) === '.json' && filepath.includes('/metadata.json')) {
+      callback(filepath);
+    }
+  });
+};
+
+walkDirectory(directoryPath, (filepath) => {
+  const content = jsonfile.readFileSync(filepath);
+  manifest.push(content);
+});
+
+jsonfile.writeFileSync('manifest.json', manifest, { spaces: 2 });
+console.log('Manifest created successfully.');
